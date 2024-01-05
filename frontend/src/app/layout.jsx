48cf7987467d715e2bc8db9490f8f02ba3ac5f28
Fiido <roccobosio@gmail.com>
2024-01-05 11:08:31 -0300

    adding font-awesome

--- frontend/src/app/layout.jsx
+++ frontend/src/app/layout.jsx
@@ -1,4 +1,7 @@
 import './globals.css'
+import "@fortawesome/fontawesome-svg-core/styles.css";
+import { config } from "@fortawesome/fontawesome-svg-core";
+config.autoAddCss = false;
 
 export const metadata = {
   title: 'HealthTech',

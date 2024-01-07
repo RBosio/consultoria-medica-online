b9f8516815b3ad2b63afa7cdff9b9d370184e307
AlanHoltz <alanholtz1999@gmail.com>
2024-01-07 14:48:23 -0300

    Seteo de cookie en el backend al autenticarse correctamente

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -1,46 +1,55 @@
-import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { UserService } from 'src/user/user.service';
 import { AuthLoginDto } from './dto/auth-login.dto';
-import { loginResponseDto } from './dto/login-response.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Doctor } from 'src/entities/doctor.entity';
 import { Repository } from 'typeorm';
 import { jwtConstants } from './constants';
+import { Response } from 'express';
 
 @Injectable()
 export class AuthService {
     constructor(
         @InjectRepository(Doctor) private doctorRepository: Repository<Doctor>,
         private userService: UserService,
-        private jwtService: JwtService) {}
-    
-    async login(userLogin: AuthLoginDto): Promise<loginResponseDto | HttpException> {
+        private jwtService: JwtService) { }
+
+    async login(userLogin: AuthLoginDto, res: Response): Promise<void> {
         const userFound = await this.userService.findOneByEmail(userLogin.email)
 
         if (await userFound.comparePassword(userLogin.password)) {
             let role = userFound.admin ? 'admin' : 'user'
 
-            if(role === 'user') {
+            if (role === 'user') {
                 const doctor = await this.doctorRepository.findOne({
                     where: {
                         userId: userFound.id
                     }
                 })
-                
-                if(doctor) {
+
+                if (doctor) {
                     role = 'doctor'
                 }
             }
 
-            const payload = { name: userFound.name, surname: userFound.surname, sub: userFound.dni, role };
+            const payload = {
+                id: userFound.id,
+                name: userFound.name,
+                surname: userFound.surname,
+                photo: userFound.image,
+                email: userFound.email,
+                dni: userFound.dni,
+                role
+            };
 
-            return {token: await this.jwtService.signAsync(payload,
-                {
-                  secret: jwtConstants.secret
-                })}
+            const token = await this.jwtService.signAsync(payload, { secret: jwtConstants.secret });
+            res.cookie("token", token, { expires: new Date(new Date().getTime() + 1000 * 60 * 60), httpOnly: true });
+            res.status(200).json({ token });
+
+
         } else {
-            throw new HttpException('Email o contraseña incorrectos', HttpStatus.UNAUTHORIZED)
+            res.status(401).json({ error: "Email o contraseña incorrectos" });
         }
     }
 }

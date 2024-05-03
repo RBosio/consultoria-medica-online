8345f1488584079363c2ce46addf5bd06589bbeb
Fiido <roccobosio@gmail.com>
2024-05-02 21:21:20 -0300

    update description, cbu and alias - backend

--- backend/src/doctor/dto/update-doctor.dto.ts
+++ backend/src/doctor/dto/update-doctor.dto.ts
@@ -1,13 +1,15 @@
-import { Speciality } from "src/entities/speciality.entity"
+import { Speciality } from 'src/entities/speciality.entity';
 
 export class updateDoctorDto {
-    registration?: string
-    title?: string
-    durationMeeting?: number
-    priceMeeting?: number
-    avgRate?: number
-    description?: string
-    planId?: number
-    userId?: number
-    specialities?: Speciality[]
+  registration?: string;
+  title?: string;
+  durationMeeting?: number;
+  priceMeeting?: number;
+  avgRate?: number;
+  description?: string;
+  planId?: number;
+  userId?: number;
+  specialities?: Speciality[];
+  cbu?: string;
+  alias?: string;
 }

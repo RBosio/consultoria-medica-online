ea8d882111be062f0776ebaeaca64ed485782ec0
Fiido <roccobosio@gmail.com>
2024-05-21 03:46:00 -0300

    fix edit plan

--- frontend/pages/admin/plans/index.tsx
+++ frontend/pages/admin/plans/index.tsx
@@ -27,8 +27,6 @@ import {
   TableHead,
   TableRow,
   Typography,
-  styled,
-  tableCellClasses,
   useTheme,
 } from "@mui/material";
 import { FaAngleRight, FaCircleInfo, FaPlus, FaXmark } from "react-icons/fa6";
@@ -48,7 +46,6 @@ interface Plan {
 }
 
 export default function Home(props: Plan) {
-  const theme = useTheme();
   const router = useRouter();
 
   const [error, setError] = useState<boolean>(false);
@@ -377,6 +374,8 @@ export default function Home(props: Plan) {
                                   setAdd(false);
                                   setO(true);
                                   setPlan(false);
+                                  editPlan.values.name = row.name;
+                                  editPlan.values.price = row.price;
                                   setPlanId(row.id);
                                 }}
                               />{" "}
@@ -399,171 +398,176 @@ export default function Home(props: Plan) {
                   </Table>
                 </TableContainer>
               </div>
-              <div>
-              </div>
+              <div></div>
             </section>
           </div>
         </div>
         <Modal
-                open={o}
-                onClose={() => setO(false)}
-                aria-labelledby="modal-modal-title"
-                aria-describedby="modal-modal-description"
-              >
-                <Fade in={o}>
-                  <Box
-                    sx={{
-                      position: "absolute" as "absolute",
-                      top: "50%",
-                      left: "50%",
-                      transform: "translate(-50%, -50%)",
-                      bgcolor: "background.paper",
+          open={o}
+          onClose={() => setO(false)}
+          aria-labelledby="modal-modal-title"
+          aria-describedby="modal-modal-description"
+        >
+          <Fade in={o}>
+            <Box
+              sx={{
+                position: "absolute" as "absolute",
+                top: "50%",
+                left: "50%",
+                transform: "translate(-50%, -50%)",
+                bgcolor: "background.paper",
 
-                      boxShadow: 24,
-                      p: 4,
-                    }}
-                  >
-                    <Typography
-                      id="modal-modal-description"
-                      component={"span"}
-                      variant={"body2"}
-                      sx={{ mt: 2 }}
+                boxShadow: 24,
+                p: 4,
+              }}
+            >
+              <Typography
+                id="modal-modal-description"
+                component={"span"}
+                variant={"body2"}
+                sx={{ mt: 2 }}
+              >
+                {add && (
+                  <div className="bg-white p-8 border border-primary rounded-md shadow-md mt-12">
+                    <h4 className="text-primary text-xl mb-4 text-center">
+                      Agregar plan
+                    </h4>
+                    <form
+                      className="flex gap-4"
+                      onSubmit={addPlan.handleSubmit}
                     >
-                      {add && (
-                        <div className="bg-white p-8 border border-primary rounded-md shadow-md mt-12">
-                        <h4 className="text-primary text-xl mb-4 text-center">
-                          Agregar plan
-                        </h4>
-                        <form className="flex gap-4" onSubmit={addPlan.handleSubmit}>
-                          <Input
-                            placeholder="Nombre"
-                            type="text"
-                            name="name"
-                            onChange={addPlan.handleChange}
-                            onBlur={addPlan.handleBlur}
-                          />
-                          <Input
-                            placeholder="Precio"
-                            type="number"
-                            name="price"
-                            onChange={addPlan.handleChange}
-                            onBlur={addPlan.handleBlur}
-                          />
-                          <Button onClick={() => setConfirm(true)}>Agregar</Button>
-                        </form>
+                      <Input
+                        placeholder="Nombre"
+                        type="text"
+                        name="name"
+                        onChange={addPlan.handleChange}
+                        onBlur={addPlan.handleBlur}
+                      />
+                      <Input
+                        placeholder="Precio"
+                        type="number"
+                        name="price"
+                        onChange={addPlan.handleChange}
+                        onBlur={addPlan.handleBlur}
+                      />
+                      <Button onClick={() => setConfirm(true)}>Agregar</Button>
+                    </form>
+                  </div>
+                )}
+                {edit && (
+                  <div className="bg-white p-8 border border-primary rounded-md shadow-md mt-12">
+                    <h4 className="text-primary text-xl mb-4 text-center">
+                      Editar plan
+                    </h4>
+                    <form
+                      className="flex gap-4"
+                      onSubmit={editPlan.handleSubmit}
+                    >
+                      <Input
+                        placeholder="Nombre"
+                        type="text"
+                        name="name"
+                        onChange={editPlan.handleChange}
+                        onBlur={editPlan.handleBlur}
+                        value={editPlan.values.name}
+                      />
+                      <Input
+                        placeholder="Precio"
+                        type="number"
+                        name="price"
+                        onChange={editPlan.handleChange}
+                        onBlur={editPlan.handleBlur}
+                        value={editPlan.values.price}
+                      />
+                      <Button onClick={() => setUpdate(true)}>Editar</Button>
+                    </form>
+                  </div>
+                )}
+                {plan && (
+                  <div className="p-2 m-4 relative">
+                    <h4
+                      className={`text-primary text-3xl text-center ${robotoBold.className}`}
+                    >
+                      {plan.name}
+                    </h4>
+                    <h3
+                      className={`text-secondary text-md text-center ${robotoBold.className} font-normal`}
+                    >
+                      $ {plan.price}
+                    </h3>
+                    <div className="text-primary text-xl flex justify-center items-center gap-4 mt-8">
+                      <h4 className="text-center underline">Beneficios</h4>
+                      <div
+                        className="hover:cursor-pointer hover:opacity-70"
+                        onClick={addBenefitsPlan}
+                      >
+                        <FaPlus />
                       </div>
-                      )}
-                      {edit && (
-                        <div className="bg-white p-8 border border-primary rounded-md shadow-md mt-12">
-                        <h4 className="text-primary text-xl mb-4 text-center">
-                          Editar plan
-                        </h4>
-                        <form className="flex gap-4" onSubmit={editPlan.handleSubmit}>
-                          <Input
-                            placeholder="Nombre"
-                            type="text"
-                            name="name"
-                            onChange={editPlan.handleChange}
-                            onBlur={editPlan.handleBlur}
-                            value={editPlan.values.name}
-                          />
-                          <Input
-                            placeholder="Precio"
-                            type="number"
-                            name="price"
-                            onChange={editPlan.handleChange}
-                            onBlur={editPlan.handleBlur}
-                            value={editPlan.values.price}
-                          />
-                          <Button onClick={() => setUpdate(true)}>Editar</Button>
-                        </form>
-                      </div>
-                      )}
-                      {plan && (
-                        <div className="p-2 m-4 relative">
-                        <h4
-                          className={`text-primary text-3xl text-center ${robotoBold.className}`}
-                        >
-                          {plan.name}
-                        </h4>
-                        <h3
-                          className={`text-secondary text-md text-center ${robotoBold.className} font-normal`}
-                        >
-                          $ {plan.price}
-                        </h3>
-                        <div className="text-primary text-xl flex justify-center items-center gap-4 mt-8">
-                          <h4 className="text-center underline">Beneficios</h4>
-                          <div
-                            className="hover:cursor-pointer hover:opacity-70"
-                            onClick={addBenefitsPlan}
-                          >
-                            <FaPlus />
-                          </div>
-                        </div>
-                        <div className="flex justify-center">
-                          <div className="md:flex md:justify-center md:items-center md:flex-wrap">
-                            {!addBenefits ? (
-                              plan.benefits.length === 0 ? (
-                                <div className="bg-secondary text-white font-semibold p-4 rounded-lg mt-4">
-                                  Actualmente no se encuentran beneficios para este
-                                  plan!
-                                </div>
-                              ) : (
-                                plan.benefits.map((b: BenefitResponseDto) => {
-                                  return (
-                                    <div
-                                      className="flex justify-between items-center gap-2 text-white bg-secondary p-4 rounded-md m-2"
-                                      key={b.id}
-                                    >
-                                      <div className="flex items-center gap-2">
-                                        <FaAngleRight /> <p>{b.name}</p>
-                                      </div>
-                                    </div>
-                                  );
-                                })
-                              )
-                            ) : (
-                              <form
-                                onSubmit={handleSubmitBenefits}
-                                className="flex flex-col"
-                              >
-                                {benefits.map((b: BenefitResponseDto) => (
-                                  <div
-                                    key={b.id}
-                                    className="flex justify-center items-center gap-2 mx-4"
-                                  >
-                                    <FormControl component="fieldset">
-                                      <div className="flex items-center gap-4">
-                                        {b.name}
-    
-                                        <FormGroup aria-label="position" row>
-                                          <FormControlLabel
-                                            control={
-                                              <Checkbox
-                                                name={b.id.toString()}
-                                                onChange={checkboxOnChange}
-                                                checked={checkeds.includes(b.id)}
-                                              />
-                                            }
-                                            label=""
-                                            labelPlacement="end"
-                                          />
-                                        </FormGroup>
-                                      </div>
-                                    </FormControl>
+                    </div>
+                    <div className="flex justify-center">
+                      <div className="md:flex md:justify-center md:items-center md:flex-wrap">
+                        {!addBenefits ? (
+                          plan.benefits.length === 0 ? (
+                            <div className="bg-secondary text-white font-semibold p-4 rounded-lg mt-4">
+                              Actualmente no se encuentran beneficios para este
+                              plan!
+                            </div>
+                          ) : (
+                            plan.benefits.map((b: BenefitResponseDto) => {
+                              return (
+                                <div
+                                  className="flex justify-between items-center gap-2 text-white bg-secondary p-4 rounded-md m-2"
+                                  key={b.id}
+                                >
+                                  <div className="flex items-center gap-2">
+                                    <FaAngleRight /> <p>{b.name}</p>
                                   </div>
-                                ))}
-                                <Button type="submit">Aceptar</Button>
-                              </form>
-                            )}
-                          </div>
-                        </div>
+                                </div>
+                              );
+                            })
+                          )
+                        ) : (
+                          <form
+                            onSubmit={handleSubmitBenefits}
+                            className="flex flex-col"
+                          >
+                            {benefits.map((b: BenefitResponseDto) => (
+                              <div
+                                key={b.id}
+                                className="flex justify-center items-center gap-2 mx-4"
+                              >
+                                <FormControl component="fieldset">
+                                  <div className="flex items-center gap-4">
+                                    {b.name}
+
+                                    <FormGroup aria-label="position" row>
+                                      <FormControlLabel
+                                        control={
+                                          <Checkbox
+                                            name={b.id.toString()}
+                                            onChange={checkboxOnChange}
+                                            checked={checkeds.includes(b.id)}
+                                          />
+                                        }
+                                        label=""
+                                        labelPlacement="end"
+                                      />
+                                    </FormGroup>
+                                  </div>
+                                </FormControl>
+                              </div>
+                            ))}
+                            <Button type="submit">Aceptar</Button>
+                          </form>
+                        )}
                       </div>
-                      )}
-                    </Typography>
-                  </Box>
-                </Fade>
-              </Modal>
+                    </div>
+                  </div>
+                )}
+              </Typography>
+            </Box>
+          </Fade>
+        </Modal>
         <Dialog
           open={confirm || update || cancel}
           onClose={() => {
